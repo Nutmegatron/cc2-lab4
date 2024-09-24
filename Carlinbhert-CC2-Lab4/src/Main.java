a62d51649dea92e385436f3094cd6a291a121be8
Nutmegatron <cadg.toralba@gmail.com>
2024-09-24 14:46:28 +0800

    fixed code to make it more readable by setting variables for the discounts

--- Carlinbhert-CC2-Lab4/src/Main.java
+++ Carlinbhert-CC2-Lab4/src/Main.java
@@ -12,20 +12,22 @@ public class Main {
         System.out.print("Enter the total purchase amount: ");
         double totalAmount = myDiscount.nextDouble();
 
+//      setting variables for the discounts
+        double firstDiscount = totalAmount * 0.05;
+        double secondDiscount = totalAmount * 0.10;
+        double thirdDiscount = totalAmount * 0.15;
+        
 //        if else statement to determine how much of a discount a person gets based on the total amount
         if (totalAmount <= 1000) {
             System.out.println("Discount applied: 0%");
             System.out.println("Final price after discount: PHP " + totalAmount);
         } else if (totalAmount >= 1000 && totalAmount <= 5000) {
-            double firstDiscount = totalAmount * 0.05;
             System.out.println("Discount applied: 5%");
             System.out.println("Final price after discount: PHP " + (totalAmount - firstDiscount));
         } else if (totalAmount >= 5001 && totalAmount <= 10000) {
-            double secondDiscount = totalAmount * 0.10;
             System.out.println("Discount applied: 10%");
             System.out.print("Final price after discount: PHP " + (totalAmount - secondDiscount));
         } else {
-            double thirdDiscount = totalAmount * 0.15;
             System.out.println("Discount applied: 15%");
             System.out.println("Final price after discount: PHP " + (totalAmount - thirdDiscount));
         }
